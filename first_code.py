c0c127e900a11dff4144f969af7c5cdab09ef33d
Qay-Tekkie <gkay260@gmail.com>
2020-03-26 13:50:00 +0100

    created a python file

---
+++ first_code.py
@@ -0,0 +1,11 @@
+print('enter a radius of a circle in the function below:')
+print('calc_area(raduis)')
+
+def calc_area(radius):
+    pi = 3.142
+    area = pi * (radius * radius)
+    print(area)
+
+
+
+    
